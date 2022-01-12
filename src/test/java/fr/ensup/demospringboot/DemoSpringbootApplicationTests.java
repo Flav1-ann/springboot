d8b68b25aff9d8cc9f8add53598904cd207ddbf3
flav1-ann <Moitoi28>
2022-01-12 18:27:28 +0100

    delete test

--- src/test/java/fr/ensup/demospringboot/DemoSpringbootApplicationTests.java
+++ src/test/java/fr/ensup/demospringboot/DemoSpringbootApplicationTests.java
@@ -1,13 +1,13 @@
-package fr.ensup.demospringboot;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoSpringbootApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package fr.ensup.demospringboot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoSpringbootApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
