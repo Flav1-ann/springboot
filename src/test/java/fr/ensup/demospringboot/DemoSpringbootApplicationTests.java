99621e9211dc3bfc5ad6622e08b689b0030c9947
flav1-ann <Moitoi28>
2022-01-12 11:17:24 +0100

    first commit

---
+++ src/test/java/fr/ensup/demospringboot/DemoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ensup.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
