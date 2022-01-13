705f9645b0ed508bdb99b879964dc07fd655a8b7
flav1-ann <Moitoi28>
2022-01-13 16:06:52 +0100

    play

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -38,9 +38,33 @@ resource "aws_security_group" "my_sg" {
   }
 
    ingress {
+    description      = "mysql from all"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "HTTP from all"
+    from_port        = 8027
+    to_port          = 8027
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
     description      = "HTTP from all"
     from_port        = 8080
     to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "HTTP from all"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
