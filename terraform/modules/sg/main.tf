95f8552ff7fd1011cadd8c38c69e56fb427611c0
MaximeDzN <maxime.dazin@gmail.com>
2022-01-14 09:05:50 +0100

    port access

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "my_sg" {
   ingress {
     from_port        = 22
     to_port          = 22
-    protocol         = "0"
+    protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
