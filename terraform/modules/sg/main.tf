951e2d584b9ee1fbc64c16afbd9c7c1f52214b2b
DAZIN Maxime <maxime.dazin@gmail.com>
2022-01-17 11:54:19 +0100

    terraform port sg

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -9,8 +9,8 @@ resource "aws_security_group" "my_sg" {
 
   #pour le HTTP pour tous
   ingress {
-    from_port        = 80
-    to_port          = 80
+    from_port        = 8080
+    to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
